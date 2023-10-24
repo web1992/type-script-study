a1b5d94c6b824c630491b0bf98df1626563f2f74
web1992 <554933654@qq.com>
2023-10-24 21:45:37 +0800

    add generator test

---
+++ generator/Generator04.js
@@ -0,0 +1,14 @@
+function* main() {
+    var result = yield request("http://some.url");
+    var resp = JSON.parse(result);
+    console.log(resp.value);
+}
+
+function request(url) {
+    makeAjaxCall(url, function(response){
+        it.next(response);
+    });
+}
+
+var it = main();
+it.next();
